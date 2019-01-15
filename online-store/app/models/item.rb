6c999530629e065bf76de7cdd15ceec01557b8b6
hejeong <jonhong777@gmail.com>
2019-01-15 16:13:56 -0500

    Add validations for creating an item

--- online-store/app/models/item.rb
+++ online-store/app/models/item.rb
@@ -2,4 +2,8 @@ class Item < ApplicationRecord
     # model relationships
     has_many :transactions
     has_many :users, through: :transactions
+
+    validates_presence_of :title
+    validates_presence_of :cost
+    validates :cost, :numericality => {:greater_than => 0}
 end
