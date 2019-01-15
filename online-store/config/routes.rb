22e2b2c1a7f9ea1185deda17bba1004e0f682913
hejeong <jonhong777@gmail.com>
2019-01-15 17:42:03 -0500

    Add welcome page with signup button

--- online-store/config/routes.rb
+++ online-store/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#home'
   get '/signup' => 'users#new'
   resources :users, only: [:create, :show]
   resources :items, only: [:new, :create, :show]
