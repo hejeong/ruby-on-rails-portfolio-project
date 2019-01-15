792e7faacb577fd2b11e39cd0da72ad1e040aa97
hejeong <jonhong777@gmail.com>
2019-01-15 15:25:11 -0500

    Add new items page

--- online-store/app/controllers/items_controller.rb
+++ online-store/app/controllers/items_controller.rb
@@ -1,3 +1,21 @@
 class ItemsController < ApplicationController
+    def new 
+        @item = Item.new
+    end
 
+    def create
+
+    end
+
+    def index
+
+    end
+
+    def show
+
+    end
+
+    def destroy
+
+    end
 end
