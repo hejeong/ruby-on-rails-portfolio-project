0725cb669d097384d90763e9089e6f27661ca2f3
hejeong <jonhong777@gmail.com>
2019-01-14 22:24:35 -0500

    Add validations for User

--- online-store/app/models/user.rb
+++ online-store/app/models/user.rb
@@ -4,4 +4,10 @@ class User < ApplicationRecord
     has_many :items, through: :transactions
 
     has_secure_password
+
+    # validations
+    validates :balance, :numericality => {:greater_than_or_equal_to => 0}
+    validates_presence_of :username
+    validates_presence_of :password
+    validates_presence_of :name
 end
