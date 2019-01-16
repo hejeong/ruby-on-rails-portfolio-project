37a68ace80cc5f431791733348d44c7ab10f9727
hejeong <jonhong777@gmail.com>
2019-01-15 20:36:17 -0500

    Complete login action

--- online-store/app/controllers/sessions_controller.rb
+++ online-store/app/controllers/sessions_controller.rb
@@ -1,20 +1,20 @@
 class SessionsController < ApplicationController
     def new
-
+        @user = User.new
     end
 
     def create
-            login_info = params[:user]
-            return redirect_to signin_path if !login_info[:username] || login_info[:username].empty? || !login_info[:password]
-            user = User.find(login_info[:username])
-            user = user.try(:authenticate, login_info[:password])
-            return redirect_to signin_path unless user
-            session[:user_id] = user.id
-            @user = user
-            redirect_to root_path
+        login_info = params[:user]
+        return redirect_to signin_path if !login_info[:username] || login_info[:username].empty? || !login_info[:password]
+        user = User.find_by(username: login_info[:username])
+        user = user.try(:authenticate, login_info[:password])
+        return redirect_to signin_path unless user
+        session[:user_id] = user.id
+        @user = user
+        redirect_to root_path
     end
 
     def destroy
-
+     
     end
 end
--- online-store/config/routes.rb
+++ online-store/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#home'
   get '/signup' => 'users#new'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
   resources :users, only: [:create, :show]
   resources :items, only: [:new, :create, :show]
 end
