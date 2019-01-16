e0ef88fba45b1a356040c4bb1ae26d7b89df5f66
hejeong <jonhong777@gmail.com>
2019-01-16 14:39:55 -0500

    Add validations for transaction

--- online-store/app/models/transaction.rb
+++ online-store/app/models/transaction.rb
@@ -2,4 +2,8 @@ class Transaction < ApplicationRecord
     # model relationships
     belongs_to :user
     belongs_to :item
+
+    # validations
+    validates_presence_of :item_id
+    validates_presence_of :user_id
 end
