c0c0dab9336b91dbbff127c018953eaab2e01ec0
hejeong <jonhong777@gmail.com>
2019-01-16 17:59:10 -0500

    Add feature for admins to edit an item listing

--- online-store/app/controllers/items_controller.rb
+++ online-store/app/controllers/items_controller.rb
@@ -13,7 +13,9 @@ class ItemsController < ApplicationController
     end 
 
     def update
-        
+        @item = Item.find(params[:id])
+        @item.update(item_params)
+        redirect_to item_path(@item)
     end
 
     def index
