d049137aefb1b4ef0ab75209877a7fcb887682b1
hejeong <jonhong777@gmail.com>
2019-01-17 15:47:55 -0500

    Add quantity field on item show page for customers to purchase more than one

--- online-store/db/schema.rb
+++ online-store/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_17_200826) do
+ActiveRecord::Schema.define(version: 2019_01_17_201707) do
 
   create_table "items", force: :cascade do |t|
     t.string "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2019_01_17_200826) do
     t.integer "item_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity"
   end
 
   create_table "users", force: :cascade do |t|
