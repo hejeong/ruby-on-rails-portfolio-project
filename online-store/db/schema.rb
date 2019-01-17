a03d0de75f0ae18e6516137607e2a9776dffe32f
hejeong <jonhong777@gmail.com>
2019-01-17 17:29:45 -0500

    Add image_url to items table

--- online-store/db/schema.rb
+++ online-store/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_17_201707) do
+ActiveRecord::Schema.define(version: 2019_01_17_222318) do
 
   create_table "items", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_01_17_201707) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "stock"
+    t.string "image_url"
   end
 
   create_table "transactions", force: :cascade do |t|
