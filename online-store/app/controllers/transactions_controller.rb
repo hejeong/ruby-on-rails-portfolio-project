ebaf12bd6b02042cd4f423cc400d39b55183057a
hejeong <jonhong777@gmail.com>
2019-01-17 15:37:53 -0500

    Update transaction create action to validate that item has enough stock and user has enough money

--- online-store/app/controllers/transactions_controller.rb
+++ online-store/app/controllers/transactions_controller.rb
@@ -1,7 +1,25 @@
 class TransactionsController < ApplicationController
     def create
-        transaction = Transaction.create(user_id: current_user.id, item_id: params[:item_id])
+        if valid_transaction?
+            transaction = Transaction.create(user_id: current_user.id, item_id: params[:item_id], quantity: params[:transaction][:quantity])
+        end
         redirect_to items_path
     end
- 
+    
+    private
+
+    def valid_transaction?
+        item = Item.find_by(id: params[:item_id])
+        quantity = params[:transaction][:quantity]
+        total_price = quantity * item.cost
+        user = current_user
+        if  quantity <= item.stock && user.balance >= total_price
+            item.stock -= quantity
+            user.balance -= total_price
+            item.save 
+            user.save
+        else
+            nil
+        end
+    end
 end
