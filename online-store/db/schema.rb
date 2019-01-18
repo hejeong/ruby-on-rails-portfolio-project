de967fb6c3bff2ce585c4c56e8153539677e3c05
hejeong <jonhong777@gmail.com>
2019-01-18 17:47:39 -0500

    Add comment model

--- online-store/db/schema.rb
+++ online-store/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_17_222318) do
+ActiveRecord::Schema.define(version: 2019_01_18_224710) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "item_id"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.string "title"
