2e8ba9621cfa92b2e7eac29a70637d9b2d37a822
hejeong <jonhong777@gmail.com>
2019-01-18 15:45:32 -0500

    Fix bug where signing in does not update session[user_id]

--- online-store/app/controllers/users_controller.rb
+++ online-store/app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
     def create
         user = User.create(user_params)
         return redirect_to signup_path unless user.valid?
+        session[:user_id] = user.id
         @user = user
         redirect_to user_path(@user)
     end
