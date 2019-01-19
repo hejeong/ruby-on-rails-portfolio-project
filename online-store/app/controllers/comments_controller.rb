ab87f0d57c62aabb061260bd7c5a1ce4ebf7e25e
hejeong <jonhong777@gmail.com>
2019-01-18 19:09:02 -0500

    Complete comment controller actions to create, display, and index comment/comments from item show page

--- online-store/app/controllers/comments_controller.rb
+++ online-store/app/controllers/comments_controller.rb
@@ -5,16 +5,19 @@ class CommentsController < ApplicationController
     end
 
     def create
-        @comment = Comment.create(user_id: current_user.id, item_id: params[:item_id], content: params[:content])
-        redirect_to item_comments(@comment)
+        @comment = Comment.create(user_id: current_user.id, item_id: params[:item_id], content: params[:comment][:content])
+        redirect_to item_comment_path(@comment.item_id,@comment)
     end
 
     def show
         @comment = Comment.find(params[:id])
+        @user = User.find_by(id: @comment.user_id)
+        @item = Item.find_by(id: @comment.item_id)
     end
 
     def index
-        @comment = Comments.where(item_id: params[:item_id])
+        @comments = Comment.where(item_id: params[:item_id])
+        @item = Item.find_by(id: params[:item_id])
     end
 
 end
