853cc0f0ef27665ae6cc7d0970b8410c6316957c
hejeong <jonhong777@gmail.com>
2019-05-29 12:01:11 -0400

    Add featured item to home page

--- online-store/app/assets/javascripts/item.js
+++ online-store/app/assets/javascripts/item.js
@@ -46,24 +46,24 @@ const bindClick = ()=>{
     })
 };
 
+
 const getItems = ()=> {
     fetch(`/items.json`)
     .then(res => res.json())
     .then(items => {
         $(".wrapper").html("");
-        let newHtml = `<a class="link back" href="/">Back</a>`
+        let newHtml = `<div class="top-div"><a class="link back" href="/">Back</a>`
         if($(".profile a").text() == "admin"){
             newHtml += `<a class="button" href="/items/new">Create Item Listing</a><br>`
         }else{
             newHtml += `<br>`
         }
-        $(".wrapper").append(newHtml);
         items.forEach(item => {
             const newItem = new Item(item);
-            newHtml = newItem.formatIndex();
-            $(".wrapper").append(newHtml);
-         
+            newHtml += newItem.formatIndex();
         })
+        newHtml += `</div>`
+        $(".wrapper").append(newHtml);
     })
 };
 
@@ -78,23 +78,24 @@ const getItemShow = (id)=>{
     })
 };
 Item.prototype.formatShow = function(){
-    let showHTML = `<img class="show-image" src="${this.imageURL}" alt="No available image"></img><h1>${this.title}</h1>
+    let showHTML = `<div class="top-div"><img class="show-image" src="${this.imageURL}" alt="No available image"></img><h1>${this.title}</h1>
                     <h3>${this.description}</h3>
                     <p>Price: $${this.cost}</p>
                     <p>Left in stock: ${this.stock}</p>`
     if($(".profile a").text() != "admin"){
-        showHTML += `<form class="new_transaction" id="new_transaction" action="/items/17/purchase" accept-charset="UTF-8" method="post"><input name="utf8" type="hidden" value="✓"><input type="hidden" name="authenticity_token" value="pFPpL4rvXm2xiJDVcZ9ibJb0gkA4pTStPm9FitW83UGLWZTiGQli2BAcfsxpvRA1e7xlrqLlBHnHUjwzreZy/g==">
+        showHTML += `<form class="new_transaction" id="new_transaction" action="/items/${this.id}/purchase" accept-charset="UTF-8" method="post"><input name="utf8" type="hidden" value="✓"><input type="hidden" name="authenticity_token" value="pFPpL4rvXm2xiJDVcZ9ibJb0gkA4pTStPm9FitW83UGLWZTiGQli2BAcfsxpvRA1e7xlrqLlBHnHUjwzreZy/g==">
         <label for="transaction_quantity">Quantity</label>
         <input value="1" type="number" name="transaction[quantity]" id="transaction_quantity">
         <a class="link back" href="/items">Back</a>  
         <input type="submit" name="commit" value="Purchase" class="button" data-disable-with="Purchase">
         </form><br><br>
         <a href="/items/${this.id}/comments/new">Create a comment</a> |
-        <a href="/items/${this.id}/comments">View comments</a>`
+        <a href="/items/${this.id}/comments">View comments</a></div>`
     }else {
         showHTML += `<a class="link back" href="/items">Back</a>
         <a class="edit" href="/items/${this.id}/edit">Edit this item</a>
-        <a class="link delete" rel="nofollow" data-method="delete" href="/items/${this.id}">Delete this item</a>`
+        <a class="link delete" rel="nofollow" data-method="delete" href="/items/${this.id}">Delete this item</a></div>`
     }
     return showHTML;
 };
+
--- online-store/app/controllers/welcome_controller.rb
+++ online-store/app/controllers/welcome_controller.rb
@@ -1,3 +1,14 @@
 class WelcomeController < ApplicationController
-  
+  def home
+    hash = Transaction.group(:item_id).sum(:quantity)
+    array_pair = find_popular_item(hash)
+    @popular_item = Item.find(array_pair.first)
+  end
+
+  private
+  def find_popular_item(hash)
+    hash.max_by do |item_id,quantity| 
+        quantity
+    end
+  end
 end
